08eadd3214d74bf35e625852bfb1c43d75fba374
Shubham Yadav <46105856+Shubham-yadav36@users.noreply.github.com>
2022-07-26 22:58:45 +0530

    initial commit

---
+++ src/main/java/com/cicddemo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello(){
+		return ResponseEntity.ok("Hello in Github Ci Integration");
+	}
+}
